2140db2fe805c8c6e60208ac4cfe0f84a51e2d57
zhulasia <132967536+zhulasia@users.noreply.github.com>
2024-10-08 19:22:58 +0800

    Update train.py

--- train.py
+++ train.py
@@ -7,5 +7,3 @@ from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
-
-法大师傅大师傅
