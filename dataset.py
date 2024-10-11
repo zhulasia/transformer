2e47ba2adba3034713b38c684001984e284b7a6a
zl <zhulasia@gmail.com>
2024-10-11 13:29:26 +0800

    updata commit

--- dataset.py
+++ dataset.py
@@ -12,5 +12,11 @@ class BilingualDataset(Dataset):
         self.src_lang=src_lang
         self.tgt_lang=tgt_lang
 
+        self.sos_token = torch.Tensor([tokenizer_src.token_to_id(['SOS'])],dtype=torch.int64)
+        self.eos_token = torch.Tensor([tokenizer_src.token_to_id(['EOS'])], dtype=torch.int64)
+        self.pad_token = torch.Tensor([tokenizer_src.token_to_id(['PAD'])], dtype=torch.int64)
+
+        def __len__(self):
+            return len(self.ds)
 
 
