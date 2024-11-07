0fe45b06923916147b20127f5737e6f7e474974e
zl <zhulasia@gmail.com>
2024-11-07 14:17:55 +0800

    initial f

--- train.py
+++ train.py
@@ -212,7 +212,7 @@ def train_model(config):
     model_filename = latest_weights_file_path(config) if preload == 'latest' else get_weights_file_path(config, preload) if preload else None
     if model_filename:
         print(f'Preloading model {model_filename}')
-        state = torch.load(model_filename)
+        state = torch.load(model_filename, map_location=device)
         model.load_state_dict(state['model_state_dict'])
         initial_epoch = state['epoch'] + 1
         optimizer.load_state_dict(state['optimizer_state_dict'])
