07a5d1e04466d149f4ebb67c63fb8bc6a711b756
zl <zhulasia@gmail.com>
2024-11-07 17:02:37 +0800

    initial f

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ def get_config():
         "model_folder": "weights",
         "model_basename": "tmodel_",
         "preload": "latest",
-        "tokenizer_file": "tokenizer_{0}.json",
+        "tokenizer_file": "vocab/tokenizer_{0}.json",
         "experiment_name": "runs/tmodel"
     }
 
